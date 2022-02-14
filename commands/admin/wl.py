48b60d79e45c09fbd5894349fa3933d56ecba7d0
KristN <kristian.kunc@gmail.com>
2022-02-14 08:32:39 +0100

    added admin check for wl

--- commands/admin/wl.py
+++ commands/admin/wl.py
@@ -11,31 +11,34 @@ class Ping(commands.Cog):
 
     @commands.command()
     async def wl(self, ctx, action: str, argument: str = None):
-        if action.lower() == "add":
-            if argument is not None:
-                lib.urls.add(argument.lower())
-                await ctx.send("Added to whitelist")
-            else:
-                await ctx.send("Please specify a url")
+        if ctx.author.id in self.config.admins:
+            if action.lower() == "add":
+                if argument is not None:
+                    lib.urls.add(argument.lower())
+                    await ctx.send("Added to whitelist")
+                else:
+                    await ctx.send("Please specify a url")
 
-        elif action.lower() == "remove":
-            if argument is not None:
-                lib.urls.remove(argument.lower())
-                await ctx.send("Removed from whitelist")
-            else:
-                await ctx.send("Please specify a url")
-        
-        elif action == "list":
-            urls = lib.urls._list()
-
-            urls_str = "```"
-            for url in urls:
-                urls_str += url + "\n"
-            urls_str += "```"
+            elif action.lower() == "remove":
+                if argument is not None:
+                    lib.urls.remove(argument.lower())
+                    await ctx.send("Removed from whitelist")
+                else:
+                    await ctx.send("Please specify a url")
             
-            await ctx.send(urls_str)
+            elif action == "list":
+                urls = lib.urls._list()
+
+                urls_str = "```"
+                for url in urls:
+                    urls_str += url + "\n"
+                urls_str += "```"
+                
+                await ctx.send(urls_str)
+            else:
+                await ctx.send("Invalid action, use `add` or `remove`")
         else:
-            await ctx.send("Invalid action, use `add` or `remove`")
+            raise commands.errors.MissingPermissions("You are not an admin")
 
 def setup(client, config):
     client.add_cog(Ping(client, config))
