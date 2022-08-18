190196cc90a49e8d914e0b2ed4ed5347682a4f20
Kristián Kunc <kristian.kunc@gmail.com>
2022-08-18 12:43:09 +0200

    token leakage oops 🚑️

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-ARG DISCORD_TOKEN
-
 FROM python:3.10-alpine
 
 COPY . /
 
 RUN pip install -r requirements.txt
 
-CMD ["python", "./main.py", "$DISCORD_TOKEN"]
+CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-import json
 import discord
 from discord.ext import commands
 
@@ -11,9 +9,4 @@ intents = discord.Intents(messages=True, members=True, guilds=True, reactions=Tr
 client = commands.Bot(command_prefix=config.prefix, intents=intents)
 cogs.load(client, config)
 
-try:
-    with open("./conf/tokens.json", "r") as f:
-        tokens = json.load(f)
-        client.run(tokens["discord"])
-except FileNotFoundError:
-    client.run(sys.argv[1])
+client.run(config.tokens.discord)
